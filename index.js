15182c0cf96151319f18f1b779bf0d530afff107
Michael JIN <emj365@gmail.com>
2018-02-09 18:41:12 +0800

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+import { ui, router as swaggerRouter, Router } from 'swagger2-koa'
+
+let router = swaggerRouter(__dirname + '/swagger.yml')
+
+router.get('/ping', async context => {
+  context.status = 200
+  context.body = {
+    serverTime: new Date().toISOString()
+  }
+})
+
+router
+  .app() // get the koa 2 server
+  .listen(3000) // start handling requests on port 3000
+console.log('done 3000')
